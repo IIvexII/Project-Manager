43b0d5b48817b3e7b842ce7958d8b7497fc560f1
Zafeer Hafeez <contact@zafeer.pk>
2023-06-03 20:03:28 +0500

    renamed:    apps/auth/__init__.py -> auth/__init__.py 	renamed:    apps/auth/admin.py -> auth/admin.py 	renamed:    apps/auth/apps.py -> auth/apps.py 	renamed:    apps/auth/migrations/__init__.py -> auth/migrations/__init__.py 	renamed:    apps/auth/models.py -> auth/models.py 	renamed:    apps/auth/tests.py -> auth/tests.py 	renamed:    apps/auth/views.py -> auth/views.py 	renamed:    apps/dashboard/__init__.py -> dashboard/__init__.py 	renamed:    apps/dashboard/admin.py -> dashboard/admin.py 	renamed:    apps/dashboard/apps.py -> dashboard/apps.py 	renamed:    apps/dashboard/migrations/__init__.py -> dashboard/migrations/__init__.py 	renamed:    apps/dashboard/models.py -> dashboard/models.py 	renamed:    apps/dashboard/tests.py -> dashboard/tests.py 	renamed:    apps/dashboard/views.py -> dashboard/views.py 	new file:   db.sqlite3 	modified:   project_manager/settings.py

--- project_manager/settings.py
+++ project_manager/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'auth.apps.AuthConfig',
+    # 'dashboard.apps.DashboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
